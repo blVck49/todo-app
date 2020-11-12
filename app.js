c7a1bc7d76c819825609cfe449e7dc32a67dc7dc
Adebiyi Temitope <adebiyitope49@gmail.com>
2020-11-12 15:06:54 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require("morgan");
 const port = 3000;
+const env = require("dotenv")
+env.config();
+
 
 //const Todo = require('./models/todo')
 const todoRoutes = require('./routes/todo');
 const userRoutes = require('./routes/user');
 
 mongoose.connect( 
-  'mongodb://127.0.0.1:27017/temp-cluster',
-  {
+ 'mongodb+srv://temp-user:process.env.DB_PASSWORD@temp-cluster.jiz6a.mongodb.net/process.env.DB_NAME?retryWrites=true&w=majority',
+ {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
